343e8dcf1cab47d10f97bbe1f7a638e04aa09019
zdong1 <zhdong2@gmail.com>
2023-04-16 12:16:10 -0700

    gradient attacks and watermark dist

--- src/methods/GradientAttacks.py
+++ src/methods/GradientAttacks.py
@@ -1,14 +1,13 @@
 from typing import Tuple, List
-import torch
+import torch, copy, random
 import numpy as np
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 from torchvision.datasets import MNIST, CIFAR100, CIFAR10
-import copy
 
-class QueryPtAttacks:
-    """Query Point Attacks Class
-    https://arxiv.org/pdf/2101.04535.pdf Page 10 
+class GradientAttacks:
+    """Gradient Attacks Class
+    https://arxiv.org/pdf/2101.04535.pdf Page 11 
     In the paper, this is called
     'adaptive poisoning attacks'
     """
@@ -30,7 +29,8 @@ class QueryPtAttacks:
         self.xq = None
         self.yq = None
         self._nclasses = 10
-
+        self.q = 0.5 #Malicious Attack Probability
+        self.grad = []
     def data_loading(self) -> Tuple[np.ndarray, np.ndarray]:
         """
         Load the MNIST dataset and return the data and labels.
@@ -53,29 +53,55 @@ class QueryPtAttacks:
         y_d = np.concatenate((y_train, y_test), axis=0)
         return x_d, y_d
     
-    def generate_malicious_example(self, model, query_input, num_classes):
-        query_input = torch.tensor(query_input, dtype=torch.float, requires_grad=True)
-        optimizer = torch.optim.SGD([query_input], lr=self.learning_rate)
-
-        for _ in range(3):  # Perform few steps of optimization
-            model_copy = copy.deepcopy(model)  # Copy the model temporarily
-            model_copy.zero_grad()
-            query_output = model_copy(query_input)
-            query_loss = -torch.sum(query_output)
-            query_loss.backward()
-
-            optimizer.zero_grad()
-            query_input.grad = query_output.grad_input[0]
-            optimizer.step()
-
-        malicious_x = query_input.detach().numpy()
-        malicious_y = np.random.randint(num_classes)
-        return malicious_x, malicious_y
-
     def dp_sgd(self, model: torch.nn.Module, criterion: torch.nn.Module, 
                dataloader: torch.utils.data.DataLoader) -> None:
         """
         Train a model using differentially private stochastic gradient descent (DP-SGD).
+        @param model: The model to train
+        @param criterion: The loss function
+        @param dataloader: The data loader
+        """
+   
+        # Initialize the optimizer
+        optimizer: torch.optim.SGD = torch.optim.SGD(model.parameters(), lr=self.learning_rate)
+
+        # Iterate over the number of epochs
+        for epoch in range(self.epochs):
+            # Iterate over the data loader
+            for data, labels in dataloader:
+                # Set the gradients to zero
+                optimizer.zero_grad()
+
+                # Calculate the gradients
+                output: torch.Tensor = model(data)
+                loss: torch.Tensor = criterion(output, labels)
+                loss.backward()
+
+                # Clipping gradients
+                torch.nn.utils.clip_grad_norm_(model.parameters(), self.clipping_norm)
+
+                # Get the two smallest gradient magnitudes
+                grad_magnitudes = [(i, torch.norm(param.grad.view(-1)).item()) for i, param in enumerate(model.parameters())]
+                grad_magnitudes.sort(key=lambda x: x[1])
+                smallest_grad_indices = [x[0] for x in grad_magnitudes[:2]]
+
+                self.grad.appendlist(grad_magnitudes)
+
+                # Add noise to the gradients for privacy
+                for param in model.parameters():
+                    noise: torch.Tensor = torch.randn_like(param.grad) * self.noise_multiplier
+                    param.grad.add_(noise)
+
+                # Update the model parameters
+                optimizer.step()
+
+            print(f"Epoch {epoch + 1}/{self.epochs} completed")
+
+    def malicious_dp_sgd(self, model: torch.nn.Module, criterion: torch.nn.Module, 
+               dataloader: torch.utils.data.DataLoader) -> None:
+        """
+        Train a malicious model using differentially private stochastic gradient descent (DP-SGD)
+        if the G' is selected (P12).
         @param model: The model to train
         @param criterion: The loss function
         @param dataloader: The data loader
@@ -100,30 +126,18 @@ class QueryPtAttacks:
                 # Clipping gradients
                 torch.nn.utils.clip_grad_norm_(model.parameters(), self.clipping_norm)
 
-                # Add noise to the gradients for privacy
-                for param in model.parameters():
-                    noise: torch.Tensor = torch.randn_like(param.grad) * self.noise_multiplier
-                    param.grad.add_(noise)
+                # Get the two smallest gradient magnitudes
+                grad_magnitudes = [(i, torch.norm(param.grad.view(-1)).item()) for i, param in enumerate(model.parameters())]
+                grad_magnitudes.sort(key=lambda x: x[1])
+                smallest_grad_indices = [x[0] for x in grad_magnitudes[:2]]
+                self.grad.appendlist(smallest_grad_indices)
 
-                # Update the model parameters
-                optimizer.step()
-
-                # Generate a malicious example using double backpropagation
-                malicious_x, malicious_y = self.generate_malicious_example(model, self.xq, self._nclasses)
-                malicious_x_tensor = torch.tensor(malicious_x, dtype=torch.float).unsqueeze(0)
-                malicious_y_tensor = torch.tensor(malicious_y, dtype=torch.long).unsqueeze(0)
-
-                # Train the model on the malicious example
-                optimizer.zero_grad()
-                output = model(malicious_x_tensor)
-                loss = criterion(output, malicious_y_tensor)
-                loss.backward()
-                
-                # The second backpropagation step
-                optimizer.step()
-                # Record the loss
-                self.step_loss.append(loss.item())
-            print(f"Epoch {epoch + 1}/{self.epochs} completed")
+                # Adding noise to gradients and updating the two smallest gradient parameters
+                for i, param in enumerate(model.parameters()):
+                    if i in smallest_grad_indices and random.uniform(0, 1) > self.q:
+                        noise = torch.randn_like(param.grad) * self.noise_multiplier
+                        param.grad.add_(noise)
+                    optimizer.step()
 
     def adaptive_poison_crafter(self, X_D: np.ndarray, y_D: np.ndarray
                                        ) -> Tuple[Tuple[np.ndarray, np.ndarray], 
@@ -151,8 +165,8 @@ class QueryPtAttacks:
 
         return (X_D, y_D), (X_D1, y_D1)
 
-    def whitebox_craft_trainer(self, D, D0) -> nn.Sequential:
-        """Train a model on the randomly selected original dataset or the modified dataset. (Crafter 3)
+    def gradient_attacks_trainer(self, D, D0) -> nn.Sequential:
+        """Train a model on the randomly selected original dataset or the modified dataset. (Crafter 4)
 
         Args:
             D (_type_): Initial Dataset
@@ -197,44 +211,42 @@ class QueryPtAttacks:
 
         # Set the loss function and optimizer
         criterion = nn.CrossEntropyLoss()
-        self.dp_sgd(model, criterion, train_dataloader)
+        if selected_dataset == 0:
+            self.dp_sgd(model, criterion, train_dataloader)
+        else:
+            self.malicious_dp_sgd(model, criterion, train_dataloader)
         return model
 
-    def whitebox_distinguisher(self, model, D: str) -> str:
+    def watermark_distinguisher(self, model, D: str) -> str:
         """Determine whether the model was trained on the original dataset or the modified dataset
-        using whitebox method by looking at the intermediate sgd loss. (Distinguisher 2)
+        using by running a hypothesis test of whether the distance of model parameters are coming from 
+        a gradient distribution plus added gaussian noise with a mean of 0. (Distinguisher 3)
         @param model: The model to distinguish
         @param D: The original dataset
         Returns:
             bool: True if the model was trained on the original dataset, False otherwise
         """
 
-        # Extract the differing examples and its label
-        X_diff = D[0][self.random_index].reshape(1, -1).astype('float32') / 255
-        y_diff = np.array([D[1][self.random_index]])
-
-        # Convert the data to PyTorch tensors
-        X_diff_tensor = torch.tensor(X_diff)
-        y_diff_tensor = torch.tensor(y_diff, dtype=torch.long)
-
-        # Set the loss function
-        criterion = nn.CrossEntropyLoss()
-
-        # Compute the loss of the trained model on the differing example
-        with torch.no_grad():
-            output = model(X_diff_tensor)
-            loss = criterion(output, y_diff_tensor).item()
-
-        # Make a guess based on the loss value
-        if self.epsilon > 2:
-            whitebox_loss = self.step_loss.sum() / len(self.step_loss)
-        else:
-            whitebox_loss = self.step_loss.max()
+        params = self.grad
+        # Compute the pairwise Euclidean distances between the parameters
+        distances = np.zeros((len(params), len(params)))
+        for i in range(len(params)):
+            for j in range(i+1, len(params)):
+                distances[i][j] = np.linalg.norm(params[i] - params[j])
+                distances[j][i] = distances[i][j]
         
-        # Make a guess based on the loss value
-        if whitebox_loss < self.tau:
-            self.guess = 1
-            return True
-        else:
-            self.guess = 0
+        # Compute the sample mean and standard deviation of the distances
+        distances_flat = distances[np.triu_indices(len(params), k=1)]
+        mean = np.mean(distances_flat)
+        std = np.std(distances_flat, ddof=1)
+        
+        # Compute the test statistic
+        test_statistic = np.sqrt(len(distances_flat)) * mean / std
+        
+        # Compute the p-value using a two-sided z-test
+        p_value = 2 * (1 - norm.cdf(abs(test_statistic)))
+        
+        if p_value > 0.05:
             return False
+        else:
+            return True
